3766124c70273639374ae7ecb0a9c1348cdb94e3
Andrey Gusakov <dron0gus@gmail.com>
2021-01-09 16:33:50 -0500

    lis302dl: fix uninitialized

--- os/ex/devices/ST/lis302dl.c
+++ os/ex/devices/ST/lis302dl.c
@@ -332,7 +332,7 @@ static msg_t acc_reset_sensivity(void *ip) {
 static msg_t acc_set_full_scale(LIS302DLDriver *devp, lis302dl_acc_fs_t fs) {
   float newfs, scale;
   uint8_t i, cr;
-  msg_t msg;
+  msg_t msg = MSG_OK;
 
   osalDbgCheck(devp != NULL);
 
@@ -354,7 +354,7 @@ static msg_t acc_set_full_scale(LIS302DLDriver *devp, lis302dl_acc_fs_t fs) {
     msg = MSG_RESET;
   }
 
-  if((msg == MSG_OK) && 
+  if((msg == MSG_OK) &&
      (newfs != devp->accfullscale)) {
     /* Computing scale value.*/
     scale = newfs / devp->accfullscale;
