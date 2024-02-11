354e29bb88feece608c61559e5bd949f18fae177
Andrey Gusakov <dron0gus@gmail.com>
2024-02-11 15:04:01 +0300

    lis3dsh: check WHO_AM_I

--- os/ex/devices/ST/lis3dsh.c
+++ os/ex/devices/ST/lis3dsh.c
@@ -464,9 +464,10 @@ void lis3dshObjectInit(LIS3DSHDriver *devp) {
  *
  * @api
  */
-void lis3dshStart(LIS3DSHDriver *devp, const LIS3DSHConfig *config) {
+msg_t lis3dshStart(LIS3DSHDriver *devp, const LIS3DSHConfig *config) {
   uint32_t i;
   uint8_t cr;
+  uint8_t devid;
   osalDbgCheck((devp != NULL) && (config != NULL));
 
   osalDbgAssert((devp->state == LIS3DSH_STOP) ||
@@ -474,6 +475,24 @@ void lis3dshStart(LIS3DSHDriver *devp, const LIS3DSHConfig *config) {
                 "lis3dshStart(), invalid state");		
 
   devp->config = config;
+
+#if LIS3DSH_USE_SPI
+#if LIS3DSH_SHARED_SPI
+  spiAcquireBus(devp->config->spip);
+#endif /* LIS3DSH_SHARED_SPI */
+  spiStart(devp->config->spip, devp->config->spicfg);
+
+
+  /* Check WHO_I_AM */
+  lis3dshSPIReadRegister(devp->config->spip, LIS3DSH_AD_WHO_AM_I,
+                         1, &devid);
+  if (devid != 0x3f)
+  {
+#if LIS3DSH_SHARED_SPI
+    spiReleaseBus(devp->config->spip);
+#endif /* LIS3DSH_SHARED_SPI */
+    return MSG_RESET;
+  }
 
   /* Control register 4 configuration block.*/
   {
@@ -483,12 +502,6 @@ void lis3dshStart(LIS3DSHDriver *devp, const LIS3DSHConfig *config) {
     cr |= devp->config->accblockdataupdate;
 #endif
   }
-
-#if LIS3DSH_USE_SPI
-#if LIS3DSH_SHARED_SPI
-  spiAcquireBus(devp->config->spip);
-#endif /* LIS3DSH_SHARED_SPI */
-  spiStart(devp->config->spip, devp->config->spicfg);
 
   lis3dshSPIWriteRegister(devp->config->spip, LIS3DSH_AD_CTRL_REG4, 1, &cr);
 
@@ -601,6 +614,8 @@ void lis3dshStart(LIS3DSHDriver *devp, const LIS3DSHConfig *config) {
   osalThreadSleepMilliseconds(10);
 
   devp->state = LIS3DSH_READY;
+
+  return MSG_OK;
 }
 
 /**
