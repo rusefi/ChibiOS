a7b11b84b609b91f57e343ae29b5dc500555365e
Andrey Gusakov <dron0gus@gmail.com>
2025-01-24 18:52:19 +0300

    rework LIMITED_WHILE_LOOP

--- os/hal/include/limited_wait.h
+++ os/hal/include/limited_wait.h
@@ -2,5 +2,5 @@
 
 // this hook allows rusEFI to fail gracefully instead of on hanging
 #ifndef LIMITED_WHILE_LOOP
-#define LIMITED_WHILE_LOOP(msg, condition) { (void)msg ; while (condition) ; }
+#define LIMITED_WHILE_LOOP(condition, ...) { while (condition) ; }
 #endif
