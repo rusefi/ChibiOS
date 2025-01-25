621dc5017d3328f48e25191491dd9ac8f9009911
Giovanni Di Sirio <gdisirio@gmail.com>
2025-01-25 08:17:51 +0000

    Fixed bug #1294.
    git-svn-id: svn://svn.code.sf.net/p/chibios/svn/trunk@16927 27425a3e-05d8-49a3-a47f-9c15f0e5edd8

--- os/hal/ports/STM32/LLD/ADCv2/hal_adc_lld.c
+++ os/hal/ports/STM32/LLD/ADCv2/hal_adc_lld.c
@@ -401,7 +401,7 @@ void adc_lld_start_conversion(ADCDriver *adcp) {
     cr2 |= ADC_CR2_DDS;
   }
 
-  /* The start method is different dependign if HW or SW triggered, the
+  /* The start method is different depending if HW or SW triggered, the
      start is performed using the method specified in the CR2 configuration.*/
   if ((cr2 & ADC_CR2_SWSTART) != 0) {
     /* Initializing CR2 while keeping ADC_CR2_SWSTART at zero.*/
@@ -425,8 +425,9 @@ void adc_lld_stop_conversion(ADCDriver *adcp) {
 
   dmaStreamDisable(adcp->dmastp);
   adcp->adc->CR1 = 0;
-  /* Because ticket #822, preserving injected conversions.*/
-  adcp->adc->CR2 &= ~(ADC_CR2_SWSTART);
+  if ((adcp->adc->CR2 & ADC_CR2_CONT) != 0U) {
+    adcp->adc->CR2 = 0U;
+  }
   adcp->adc->CR2 = ADC_CR2_ADON;
 }
 
